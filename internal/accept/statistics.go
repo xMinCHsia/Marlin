1973fd6c5b0811c4113a3f2466fd37bc036b827e
xMinCHsia <7981354+xMinCHsia@users.noreply.github.com>
2016-08-08 17:07:00 +0900

    fix: draft-models

--- internal/accept/statistics.go
+++ internal/accept/statistics.go
@@ -7,3 +7,7 @@ import "sync"
 type Window struct {
 	mu     sync.Mutex
 	size   int
+	events map[string][]bool // true = accepted
+}
+
+// NewWindow builds a window that remembers the last size events per draft.
