c9dbf219c7087465701142430729433b1e0713fb
xMinCHsia <7981354+xMinCHsia@users.noreply.github.com>
2017-01-13 17:08:00 +0900

    fix: correct kv-cache

--- internal/draft/ensemble.go
+++ internal/draft/ensemble.go
@@ -9,3 +9,9 @@ import (
 	"github.com/xMinCHsia/marlin/internal/config"
 )
 
+// Ensemble holds the draft models and their current weights.
+type Ensemble struct {
+	models  []config.DraftModel
+	weights map[string]float64
+}
+
