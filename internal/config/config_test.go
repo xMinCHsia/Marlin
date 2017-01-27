73f78ebd704aeee58af63dbfc96a36e24b8a66f3
xMinCHsia <7981354+xMinCHsia@users.noreply.github.com>
2017-01-27 10:52:00 +0900

    fix: correct performance

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -8,3 +8,9 @@ import (
 )
 
 func write(t *testing.T, body string) string {
+	p := filepath.Join(t.TempDir(), "m.yaml")
+	if err := os.WriteFile(p, []byte(body), 0o600); err != nil {
+		t.Fatal(err)
+	}
+	return p
+}
