5cc18b5ab0358f710f39057a146a4f20ea212c83
xMinCHsia <7981354+xMinCHsia@users.noreply.github.com>
2017-02-28 16:21:00 +0900

    chore: developer-tools

--- internal/draft/sampler.go
+++ internal/draft/sampler.go
@@ -2,3 +2,6 @@
 package draft
 
 import "math/rand"
+
+// Sampler draws token proposals from a draft model's logits-like scores.
+// In production the daemon receives token ids from the draft endpoint;
