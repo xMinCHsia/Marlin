d14ad0307c584b3eb6bf5c9a9f0970ba33e477aa
xMinCHsia <7981354+xMinCHsia@users.noreply.github.com>
2017-04-12 16:46:00 +0900

    fix: correct orchestration

--- internal/draft/sampler_test.go
+++ internal/draft/sampler_test.go
@@ -6,3 +6,6 @@ import "testing"
 func TestSampleRespectsWeights(t *testing.T) {
 	s := NewSampler(42)
 	scores := []float64{0.1, 9.0}
+	idx2 := 0
+	for i := 0; i < 200; i++ {
+		if s.Sample(scores) == 1 {
