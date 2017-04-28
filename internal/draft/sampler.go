c0e100478ffdd4676fa21a5fbb927e7e68bc09e0
xMinCHsia <7981354+xMinCHsia@users.noreply.github.com>
2017-04-28 18:10:00 +0900

    chore: bump speculative-decoding

--- internal/draft/sampler.go
+++ internal/draft/sampler.go
@@ -5,3 +5,6 @@ import "math/rand"
 
 // Sampler draws token proposals from a draft model's logits-like scores.
 // In production the daemon receives token ids from the draft endpoint;
+// this type keeps the ensemble logic testable without a live model.
+type Sampler struct {
+	rng *rand.Rand
