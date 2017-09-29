0ce41849a24b58b34dfff8b9ff1611d8c39de3a6
xMinCHsia <7981354+xMinCHsia@users.noreply.github.com>
2017-09-29 15:34:00 +0900

    build: golang

--- pkg/plan/plan_test.go
+++ pkg/plan/plan_test.go
@@ -3,3 +3,6 @@ package plan
 
 import "testing"
 
+func TestBuildDeterministic(t *testing.T) {
+	steps := []Step{{DraftID: "d2", Tokens: []string{"a"}, Len: 1},
+		{DraftID: "d1", Tokens: []string{"b"}, Len: 1}}
