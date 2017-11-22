c46c57d2ef0686df1eb83ce01c37645b62cf01fd
xMinCHsia <7981354+xMinCHsia@users.noreply.github.com>
2017-11-22 09:23:00 +0900

    chore: golang

--- pkg/plan/plan_test.go
+++ pkg/plan/plan_test.go
@@ -6,3 +6,6 @@ import "testing"
 func TestBuildDeterministic(t *testing.T) {
 	steps := []Step{{DraftID: "d2", Tokens: []string{"a"}, Len: 1},
 		{DraftID: "d1", Tokens: []string{"b"}, Len: 1}}
+	p1 := Build("r1", "t", 2, steps)
+	p2 := Build("r1", "t", 2, steps)
+	if p1.SummaryHash != p2.SummaryHash {
