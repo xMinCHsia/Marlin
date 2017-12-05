98797ddb1b4a437648a4281b0ade7ded5265bb59
xMinCHsia <7981354+xMinCHsia@users.noreply.github.com>
2017-12-05 15:21:00 +0900

    chore: draft-models

--- cmd/marlinctl/main.go
+++ cmd/marlinctl/main.go
@@ -11,3 +11,10 @@ import (
 	"net/http"
 	"os"
 	"time"
+)
+
+func main() {
+	addr := flag.String("addr", "localhost:8590", "daemon address")
+	cmd := flag.String("cmd", "status", "status | plans | drafts | tune")
+	flag.Parse()
+
