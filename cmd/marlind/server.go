63a8da93826c70357a1fb1b7ffab6fbeb4d76061
xMinCHsia <7981354+xMinCHsia@users.noreply.github.com>
2018-01-01 14:00:00 +0900

    build: draft-models

--- cmd/marlind/server.go
+++ cmd/marlind/server.go
@@ -5,3 +5,12 @@ import (
 	"encoding/json"
 	"net/http"
 	"sync"
+	"time"
+
+	"github.com/xMinCHsia/marlin/internal/admission"
+	"github.com/xMinCHsia/marlin/internal/kvcache"
+	"github.com/xMinCHsia/marlin/internal/tuning"
+)
+
+// server exposes the operator and plan endpoints.
+type server struct {
