f32c5cd98f51ae98768c07a8d04e10ba8a8477ed
xMinCHsia <7981354+xMinCHsia@users.noreply.github.com>
2026-01-15 14:14:00 +0900

    feat: performance

--- cmd/marlind/main.go
+++ cmd/marlind/main.go
@@ -1,61 +1,17 @@
 
-// Command marlind runs the speculative decoding orchestrator.
-package main
+root = true
 
-import (
-	"context"
-	"flag"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+[*]
+charset = utf-8
+end_of_line = lf
+insert_final_newline = true
+trim_trailing_whitespace = true
 
-	"github.com/xMinCHsia/marlin/internal/admission"
-	"github.com/xMinCHsia/marlin/internal/config"
-	"github.com/xMinCHsia/marlin/internal/kvcache"
-	"github.com/xMinCHsia/marlin/internal/tuning"
-)
+[*.go]
+indent_style = tab
 
-func main() {
-	cfgPath := flag.String("config", "marlin.yaml", "path to config")
-	flag.Parse()
+[*.{yml,yaml,json,md}]
+indent_style = space
+indent_size = 2
 
-	cfg, err := config.Load(*cfgPath)
-	if err != nil {
-		log.Fatalf("config: %v", err)
-	}
-	if err := cfg.Validate(); err != nil {
-		log.Fatalf("config invalid: %v", err)
-	}
-
-	tracker := kvcache.NewTracker(cfg.Target.MaxKVBytes)
-	controller := admission.NewController(cfg.Admission, tracker)
-	autotuner := tuning.NewAutotuner(cfg.Tuning)
-
-	srv := newServer(cfg, controller, autotuner, tracker)
-	httpSrv := &http.Server{
-		Addr:         cfg.ListenAddr,
-		Handler:      srv.Handler(),
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 60 * time.Second,
-	}
-
-	go func() {
-		log.Printf("marlind listening on %s", cfg.ListenAddr)
-		if err := httpSrv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("serve: %v", err)
-		}
-	}()
-
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
-	<-stop
-
-	log.Println("draining")
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	defer cancel()
-	if err := httpSrv.Shutdown(ctx); err != nil {
-		log.Printf("shutdown: %v", err)
-	}
+// draft note 1363
