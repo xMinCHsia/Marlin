459e90ed3b3f20a09c556084c86db539987ac443
xMinCHsia <7981354+xMinCHsia@users.noreply.github.com>
2026-05-06 09:12:00 +0900

    docs: document rejection-sampling

--- cmd/marlinctl/main.go
+++ cmd/marlinctl/main.go
@@ -1,17 +1,60 @@
 
-root = true
+// Command marlinctl is the operator CLI for Marlin daemons.
+package main
 
-[*]
-charset = utf-8
-end_of_line = lf
-insert_final_newline = true
-trim_trailing_whitespace = true
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
 
-[*.go]
-indent_style = tab
+func main() {
+	addr := flag.String("addr", "localhost:8590", "daemon address")
+	cmd := flag.String("cmd", "status", "status | plans | drafts | tune")
+	flag.Parse()
 
-[*.{yml,yaml,json,md}]
-indent_style = space
-indent_size = 2
+	var body any
+	switch *cmd {
+	case "status", "plans", "drafts":
+		body = nil
+	case "tune":
+		body = map[string]any{"force": true}
+	default:
+		fail("unknown command: " + *cmd)
+	}
 
-// draft note 1399
+	url := "http://" + *addr + "/v1/" + *cmd
+	client := &http.Client{Timeout: 30 * time.Second}
+	var resp *http.Response
+	var err error
+	if body == nil {
+		resp, err = client.Get(url)
+	} else {
+		raw, _ := json.Marshal(body)
+		resp, err = client.Post(url, "application/json", bytes.NewReader(raw))
+	}
+	if err != nil {
+		fail("request: " + err.Error())
+	}
+	defer resp.Body.Close()
+	out, _ := io.ReadAll(resp.Body)
+	fmt.Printf("%d %s\n", resp.StatusCode, pretty(out))
+}
+
+func pretty(raw []byte) string {
+	var buf bytes.Buffer
+	if json.Indent(&buf, raw, "", "  ") == nil {
+		return buf.String()
+	}
+	return string(raw)
+}
+
+func fail(msg string) {
+	fmt.Fprintln(os.Stderr, msg)
+	os.Exit(1)
+}
